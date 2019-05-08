c040ecc62dfb224f1b6fd927bb00a3184e047bb4
Garrett Estrin <garrett.estrin@gmail.com>
2019-05-07 21:45:24 -0700

    actually add button

--- routes/clapifyRoutes.js
+++ routes/clapifyRoutes.js
@@ -9,14 +9,42 @@ router.route('/')
     res.status(200).json({
       status: "ok",
       text: clapifiedString,
-      "actions": [
+      "attachments": [
         {
-            "name": "clap",
-            "text": "Clap Em'",
-            "type": "button",
-            "value": "clap"
-        },
-      ]
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "clap",
+                    "text": "Clap Em'",
+                    "type": "button",
+                    "value": "clap"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
       // response_type: "in_channel",
     })
   })
