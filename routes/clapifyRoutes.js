ae579acb220ec4f827f070662c6eafc5c5c2f7cb
Garrett Estrin <garrett.estrin@gmail.com>
2019-05-11 00:12:10 -0700

    cleaner usage

--- routes/clapifyRoutes.js
+++ routes/clapifyRoutes.js
@@ -1,6 +1,8 @@
 const 
     express = require('express'),
-    router = express.Router()
+    router = express.Router(),
+    axios = require('axios'),
+    dotenv = require('dotenv').config()
 
 router.route('/')
   .post(function(req, res){
@@ -8,48 +10,33 @@ router.route('/')
     let clapifiedString = clapify(req.body.text);
     res.status(200).json({
       status: "ok",
-      text: clapifiedString,
-      "attachments": [
-        {
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "clap",
-                    "text": "Clap Em'",
-                    "type": "button",
-                    "value": clapifiedString
-                },
-                {
-                    "name": "cancel",
-                    "text": "Show Mercy",
-                    "type": "button",
-                    "value": "cancel"
-                },
-            ]
-        }
-    ]
-      // response_type: "in_channel",
+      response_type: "ethereal",
+      text: "You clapped em' 👏"
     })
+    let postUrl = buildMessageUrl(clapifiedString, req.body.user_id, req.body.channel_id, process.env.clappifyToken);
+    axios.post(postUrl)
   })
 
+// User this route to handle interactive messages
+// Not used currently
 router.route('/post')
   .post(function(req, res){
-    console.log(req.body.actions);
+    let data = JSON.parse(req.body.payload);
+    // console.log(data);
+    let text = data.actions[0].value;
     res.status(200).json({
       status: "ok",
-      text: "clapifiedString",
-      response_type: "in_channel",
+      // text: text,
+      response_type: "ethereal",
     })
+    let postUrl = buildMessageUrl(text, data.user.id, data.channel.id, process.env.token);
+    axios.post(postUrl)
+      .then((response) => {
+        // console.log({response});
+      })
   })
 
-
-
-
-
 module.exports = router
-
 
 function clapify(string){
 	let clapifiedString = string.replaceAll(" ", " 👏 ");
@@ -61,3 +48,7 @@ String.prototype.replaceAll = function(search, replacement) {
   return target.replace(new RegExp(search, 'g'), replacement);
 };
 
+function buildMessageUrl(message, user, channel, token){
+  let encodedMessage = encodeURIComponent(message);
+  return `https://slack.com/api/chat.postMessage?token=${token}&channel=${channel}&text=${encodedMessage}&as_user=true&pretty=1`
+}
